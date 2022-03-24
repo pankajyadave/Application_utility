f5bd540a9a98e687d702c493309689bd379314af
pankajyadave <87933663+pankajyadave@users.noreply.github.com>
2022-03-24 11:44:07 +0530

    Update Applicationutility.java

--- Applicationutility.java
+++ Applicationutility.java
@@ -5,8 +5,16 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -127,6 +135,64 @@ public class Application_utility extends Base_library
 			
 		} catch (Exception e) {
 			System.out.println("Issue in Dot_Click_Using_JavascriptExecutor "+e);
+		}
+	}
+	//check broken link
+	public static void Broken_Link()
+	{
+		try {
+			String current_url=driver.getCurrentUrl();
+			String url = "";
+	        HttpURLConnection huc = null;
+	        int respCode = 200;
+	        
+	        List<WebElement> links = driver.findElements(By.tagName("a"));
+	        
+	        Iterator<WebElement> it = links.iterator();
+	        
+	        while(it.hasNext()){
+	            
+	            url = it.next().getAttribute("href");
+	            
+	            System.out.println(url);
+	        
+	            if(url == null || url.isEmpty()){
+	System.out.println("URL is either not configured for anchor tag or it is empty");
+	                continue;
+	            }
+	            
+	            if(!url.startsWith(current_url)){
+	                System.out.println("URL belongs to another domain, skipping it.");
+	                continue;
+	            }
+	            
+	            try {
+	                huc = (HttpURLConnection)(new URL(url).openConnection());
+	                
+	                huc.setRequestMethod("HEAD");
+	                
+	                huc.connect();
+	                
+	                respCode = huc.getResponseCode();
+	                
+	                if(respCode >= 400){
+	                    System.out.println(url+" is a broken link");
+	                }
+	                else{
+	                    System.out.println(url+" is a valid link");
+	                }
+	                    
+	            } catch (MalformedURLException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            } catch (IOException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	        }
+			
+		} catch (Exception e) {
+			System.out.println("Issue in Broken_Link "+e);
 		}
 	}
 }
